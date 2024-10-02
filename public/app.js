63921c22efc1f4c15942354c926a87c1bdc1b0b3
VonRiddarn <vonriddarn@gmail.com>
2024-10-02 15:26:02 +0200

    feat: added enums for Colors and Attributes pair-programmers: Stephanie, Alex, Mattias and Timmy

---
+++ public/app.js
@@ -0,0 +1,22 @@
+// Color enum
+const MonsterColor = Object.freeze
+(
+	{
+		Blue: "blue",
+		Green: "green",	
+		Red: "red",
+		Purple: "purple",
+	}
+);
+
+// Attributes enum
+const MonsterAttribute = Object.freeze
+(
+	{
+		Horns: "horns",
+		Eyes: "eyes",
+		Wings: "wings",
+		Tentacles: "tentacles",
+		Tails: "tails",
+	}
+);
