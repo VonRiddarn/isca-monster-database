76b17810ca46402d786f8a8152cbd72e704f6405
Vonriddarn <vonriddarn@gmail.com>
2024-10-03 01:19:54 +0200

    feat: created dummy file for the filtered search with file information todo: actually start programming the file

---
+++ public/scripts/filtered_search.js
@@ -0,0 +1,6 @@
+// filtered_search.js
+//
+// This file contains all variables and functionality related to the filtered search.
+// This script should remain self-contained!
+
+//todo: make the script
