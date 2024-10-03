ca03154d37c28458a877b1e9cd718080baa87e4f
Vonriddarn <vonriddarn@gmail.com>
2024-10-03 13:44:22 +0200

    feat: added monster attributes to the filtered search

--- public/scripts/filtered_search.js
+++ public/scripts/filtered_search.js
@@ -19,39 +19,77 @@ filteredSearchRoot.innerHTML = "";
 
 // Create <form> then create <span> and save the <span> to a variable so we can mutate it in a function later.
 const filteredSearchForm = filteredSearchRoot.appendChild(document.createElement("form"));
-filteredSearchForm.setAttribute("id", "filter-search-monster-form");
+filteredSearchForm.setAttribute("style", "display: flex; flex-direction: column"); // DEBUG!!!! EXPERIMENTAL!!! REMOVE WHEN WE HAVE CSS!!!
+console.warn(`DEBUG CODE IS STILL IN USE: ${filteredSearchRoot.className} form style is set inside javascript!`);
 
 // Add form elements to span through functions
-applyMonsterColorSpanToForm(filteredSearchForm);
+applyMonsterColorToForm(filteredSearchForm);
+applyMonsterAttributesToForm(filteredSearchForm);
 
 
-// Functions
-function applyMonsterColorSpanToForm(form)
+/**
+ * Generates and applies MonsterColor HTML elements the passed form.
+ * @param {Form} form 
+ */
+function applyMonsterColorToForm(form)
 {
-	// Initialize <span> element
+	// <span>
 	const span = form.appendChild(document.createElement("span"));
+	span.setAttribute("id", "filtered-search-monster-color-span");
 
-	// Initialize <label> element
+	// <input type="checkbox">
+	const colorCheckBox = span.appendChild(document.createElement("input"));
+	colorCheckBox.setAttribute("type", "checkbox");
+	colorCheckBox.setAttribute("id", "filtered-search-monster-color-isactive");
+
+	// <label>
 	const label = span.appendChild(document.createElement("label"));
-	label.setAttribute("for", "filter-search-monster-form-color");
+	label.setAttribute("for", "filtered-search-monster-color-isactive");
 	label.innerHTML = "Color";
 
-	//Initialize <select>
-	const select = span.appendChild(document.createElement("select"));
-	select.setAttribute("id", "filter-search-monster-form-color");
+	// <select>
+	addDropDownFromEnum(span, MonsterColor, "filtered-search-monster-color-dropdown");
+}
 
-	// Add all color options to select
-	for (const color in MonsterColor) 
+/**
+ * Generates and applies MonsterAttribute HTML elements the passed form.
+ * @param {Form} form 
+ */
+function applyMonsterAttributesToForm(form)
+{
+	for (const attribute in MonsterAttribute) 
 	{
-		let opt = select.appendChild(document.createElement("option"));
-		opt.setAttribute("value", `${color}`);
-		opt.innerHTML = `${MonsterColor[color]}`;
+		const idRoot = `filtered-search-monster-attribute-${MonsterAttribute[attribute]}`;
+
+		// <span> GENERIC
+		const span = form.appendChild(document.createElement("span"));
+		span.setAttribute("class", "filtered-search-monster-attribute-span");
+		span.setAttribute("display", "flex"); // EXPERIMENTAL AF, DEBUG!!!!! REMOVE WHEN WE ARE WORKING ON CSS!!!!!
+		console.warn(`DEBUG CODE IS STILL IN USE: ${idRoot} span style is set inside javascript!`);
+
+		// <input type="checkbox" />
+		const checkbox = span.appendChild(document.createElement("input"));
+		checkbox.setAttribute("type", "checkbox");
+		checkbox.setAttribute("id", `${idRoot}-isactive`);
+
+		// <label>
+		const label = span.appendChild(document.createElement("label"));
+		label.setAttribute("for", `${idRoot}-isactive`)
+		label.innerHTML = `${MonsterAttribute[attribute]}`;
+
+		// <select>
+		addDropDownFromEnum(span, NumerixFilterMethod, `${idRoot}-numeric-filter-dropdown`);
+
+		// <input type="number" />
+		const count = span.appendChild(document.createElement("input"));
+		count.setAttribute("type", "number");
+		count.setAttribute("id", `${idRoot}-count`);
 	}
 }
 
-
-
-/*<form>
+/*
+<form>
+	<!-- COLOR -->
 	<span>
 		<label for="filter-search-form-color">Color</label>
 		<select id="filter-search-form-color">
@@ -60,4 +98,17 @@ function applyMonsterColorSpanToForm(form)
 			<option value="Blue">Blue</option>
 		</select>
 	</span>
-</form>*/
+
+	<!-- Attributes -->
+	<span style="display: flex;">
+		<input type="checkbox" id="foo"/>
+		<label for="foo">Bar</label>
+		<select name="dropdown-numeric-filter" id="numeric-filter-bar">
+			<option value="LessThan">&lt;</option>
+			<option value="Equals">=</option>
+			<option value="GreaterThan">&gt;</option>
+		</select>
+		<input type="number" />
+	</span>
+</form>
+*/
