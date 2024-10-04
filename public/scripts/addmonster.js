d0313109be27c574bfdfe44e9945dfd1f424d144
mLordswe <alexander.csore@gmail.com>
2024-10-04 14:59:26 +0200

    fix: added static form to html, manipulate form in addmonster.js

--- public/scripts/addmonster.js
+++ public/scripts/addmonster.js
@@ -1,19 +1,23 @@
 const addMonsterRoot = document.querySelector(".add-monster");
-addMonsterRoot.innerHTML = "";
+
 //Creates <form>
-const monsterForm = addMonsterRoot.appendChild(document.createElement("form"));
-monsterForm.setAttribute("id", "monster-form");
-//Creates <div> for text input field.
-const monsterDiv = monsterForm.appendChild(document.createElement("div"));
+const monsterForm = document
+  .querySelector(".add-monster")
+  .querySelector("form");
+
+//Creates <span> for text input field.
+const monsterSpan = monsterForm.appendChild(document.createElement("span"));
 //Creates <label> for text input and a text field
-const monsterNameLabel = monsterDiv.appendChild(
+const monsterNameLabel = monsterSpan.appendChild(
   document.createElement("label")
 );
 
-const monsterTextInput = monsterDiv.appendChild(
+const monsterTextInput = monsterSpan.appendChild(
   document.createElement("input")
 );
+
 monsterTextInput.setAttribute("id", "monster-text-input");
+
 monsterNameLabel.setAttribute("for", "monster-text-input");
 monsterNameLabel.innerHTML = "Name: ";
 //creates a label with a corresponding dropdown menu for color
@@ -27,9 +31,26 @@ addDropDownFromEnum(monsterForm, MonsterColor, "monster-color");
 monsterForm.appendChild(document.createElement("br")); // temporary linebreak
 
 //creates dropdown for monster attributes
-const monsterAttributeLabel = monsterForm.appendChild(
-  document.createElement("label")
+// const monsterAttributeLabel = monsterForm.appendChild(
+//   document.createElement("label")
+
+// );
+// monsterAttributeLabel.setAttribute("for", "monster-attribute");
+// monsterAttributeLabel.innerHTML = "Attributes: ";
+// addDropDownFromEnum(monsterForm, MonsterAttribute, "monster-attribute");
+
+//kunna ta värdet från attributes. lägga till en input text för
+addNumericInputFieldsFromEnum(
+  monsterForm,
+  MonsterAttribute,
+  "add-monster-attributes",
+  false
 );
-monsterAttributeLabel.setAttribute("for", "monster-attribute");
-monsterAttributeLabel.innerHTML = "Attributes: ";
-addDropDownFromEnum(monsterForm, MonsterAttribute, "monster-attribute");
+
+const addMonsterBtn = monsterForm.appendChild(document.createElement("button"));
+addMonsterBtn.setAttribute("type", "submit");
+addMonsterBtn.setAttribute("onclick", "return false");
+addMonsterBtn.setAttribute("id", "add-monster-btn");
+addMonsterBtn.innerHTML = "Add monster";
+
+addMonsterBtn.addEventListener("click", function logData(params) {});
