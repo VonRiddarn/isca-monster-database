02e584a174a5e9a5d7ab61ad101e27c17df65e84
VonRiddarn <vonriddarn@gmail.com>
2024-10-04 09:57:14 +0200

    feat: added function to dynamically add numreic inputs from enums

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -23,3 +23,46 @@ function addDropDownFromEnum(parentElement, enumToItterate, id)
 		opt.innerHTML = `${enumToItterate[e]}`;
 	}
 }
+
+/**
+ * 
+ * @param {Element} parentElement The element on where to apply this list of numeric inputs.
+ * @param {"Object"} enumToItterate The enum / object from where to get the keys.
+ * @param {String} idRoot The root name for the created elements. eg: filtered-search-form
+ * @param {Boolean} isFilter Adds a checkbox and numericFilterEnum to the input.
+ */
+function addNumericInputFieldsFromEnum(parentElement, enumToItterate, idRoot, isFilter = false)
+{
+	for (const el in enumToItterate) 
+	{
+		const elementId = `${idRoot}-${enumToItterate[el]}`;
+
+		// <span> GENERIC
+		const span = parentElement.appendChild(document.createElement("span"));
+		span.setAttribute("class", `${idRoot}-span`);
+		span.setAttribute("display", "flex"); // EXPERIMENTAL AF, DEBUG!!!!! REMOVE WHEN WE ARE WORKING ON CSS!!!!!
+		console.warn(`DEBUG CODE IS STILL IN USE: ${idRoot} span style is set inside javascript!`);
+
+		// <input type="checkbox" />
+		if(isFilter)
+		{
+			const checkbox = span.appendChild(document.createElement("input"));
+			checkbox.setAttribute("type", "checkbox");
+			checkbox.setAttribute("id", `${elementId}-isactive`);
+		}
+
+		// <label>
+		const label = span.appendChild(document.createElement("label"));
+		label.setAttribute("for", `${elementId}-${isFilter ? "isactive" : "count"}`);
+		label.innerHTML = `${enumToItterate[el]}`;
+
+		// <select>
+		if(isFilter)
+			addDropDownFromEnum(span, NumerixFilterMethod, `${elementId}-numeric-filter-dropdown`);
+
+		// <input type="number" />
+		const count = span.appendChild(document.createElement("input"));
+		count.setAttribute("type", "number");
+		count.setAttribute("id", `${elementId}-count`);
+	}
+}
