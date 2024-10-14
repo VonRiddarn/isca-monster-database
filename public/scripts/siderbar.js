1154445a07097106d9fd0eca3fa98740b73ec089
tokojkis <alexander.csore@gmail.com>
2024-10-14 17:49:28 +0200

    renamed some functions for easier readability

--- public/scripts/siderbar.js
+++ public/scripts/siderbar.js
@@ -1,22 +1,4 @@
-document.querySelector("main");
-
-let isShown = null;
-
-function showBtn() {
-  document.querySelector("aside").style.width = "250 px";
-  isShown = true;
-  console.log(isShown);
-}
-
-function hideBtn() {
-  document.querySelector("aside").style.width = "0px";
-  isShown = false;
-  console.log(isShown);
-}
-
-const button = document.querySelector(".hamburger");
-
-function myFunction() {
+function formButton() {
   var x = document.querySelector("aside");
   if (x.style.display === "none") {
     x.style.display = "block";
