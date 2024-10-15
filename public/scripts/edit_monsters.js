2bc518f62ca3bb88138cc4528339fc72c50b29d6
VonRiddarn <vonriddarn@gmail.com>
2024-10-15 19:33:49 +0200

    feat: added edit mode for monster cards

--- public/scripts/edit_monsters.js
+++ public/scripts/edit_monsters.js
@@ -72,10 +72,53 @@ function enterEditMode(articleElement, monsterObject)
 	}
 }
 
-function exitEditMode(card, saveChanges)
+function exitEditMode(articleElement, saveChanges, monsterObject)
 {
-	toggleButtonSpans(card, false);
+	toggleButtonSpans(articleElement, false);
+
+	if(!saveChanges)
+	{
+		replaceCard(articleElement, monsterObject);
+		return monsterObject;
+	}
+
+	const mockMonster = {};
+	mockMonster.uid = monsterObject.uid;
+	mockMonster.stats = {};
+
+	const sections = articleElement.querySelectorAll("section");
+
+	const aliasElement = sections[0].children[1].querySelector("input");
+	mockMonster.alias = saveChanges ? aliasElement.value : monsterObject.alias;
 	
+	const colorElement = sections[0].children[2].querySelector("select");
+	mockMonster.color = saveChanges ? colorElement.value : monsterObject.color;
+
+	const attributeSpans = articleElement.querySelector("ul").children;
+
+	for (let i = 0; i < attributeSpans.length; i++) 
+	{
+		const attributeElement = attributeSpans[i].querySelector("input");
+		const attributeName = attributeSpans[i].querySelector("label").innerHTML;
+		mockMonster.stats[attributeName] = saveChanges ? attributeElement.value : monsterObject.stats[attributeName];
+	}
+
+	// Apply to object
+	const mon = monsters.find((monster) => monster.uid === mockMonster.uid);
+
+	for (const key in mon) 
+	{
+    	mon[key] = mockMonster[key];
+  	}
+
+	replaceCard(articleElement, mockMonster);
+	return mockMonster;
+}
+
+function replaceCard(card, newMonsterObject)
+{
+	card.innerHTML = "";
+	generateCardContent(card, newMonsterObject);
 }
 
 function deleteCard(card, object)
@@ -88,3 +131,107 @@ function deleteCard(card, object)
 	console.log("--- --- ---");
 }
 
+function generateCardContent(monsterCard, monster)
+{
+
+	////////////// IGNORERA KODEN HÄRIFRÅN, TILL - ////////////////////////////
+	// DELETE / EDIT buttons
+	const deleteEditSpan = monsterCard.appendChild(document.createElement("span"));
+
+	const deleteButton = deleteEditSpan.appendChild(document.createElement("button"));
+	deleteButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		deleteCard(monsterCard, monster);
+
+	});
+	deleteButton.innerHTML = "Delete";
+
+	const editButton = deleteEditSpan.appendChild(document.createElement("button"));
+	editButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		enterEditMode(monsterCard, monster);
+
+	});
+	editButton.innerHTML = "Edit";
+
+	// SAVE / CANCEL buttons
+	const saveCancelSpan = monsterCard.appendChild(document.createElement("span"));
+	saveCancelSpan.style = "display: none;";
+
+	const saveButton = saveCancelSpan.appendChild(document.createElement("button"));
+	saveButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		exitEditMode(monsterCard, true, monster);
+
+	});
+	saveButton.innerHTML = "Save";
+
+	const cancelButton = saveCancelSpan.appendChild(document.createElement("button"));
+	cancelButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		exitEditMode(monsterCard, false, monster);
+		console.warn("EDIT BUTTON CURRENTLY DELETES MONSTER - ADD FUNCTION TO EDIT!!!");
+
+	});
+	cancelButton.innerHTML = "Cancel";
+	////////////////////////// HIT /////////////////////////////////
+	// Förklaring:
+	// Denna koden lägger till knapparna för att redigera och ta bort monster
+	// Den "måste" ligga i 
+
+
+	// create section containing monster alias
+	const monsterProfile = document.createElement("section");
+	monsterProfile.className = "monster-profile";
+	monsterCard.appendChild(monsterProfile);
+
+	const monsterImage = document.createElement("section");
+	monsterImage.className = "monster-image";
+	monsterImage.innerHTML = ("");
+	monsterProfile.appendChild(monsterImage);
+
+	const monsterAlias = monsterProfile.appendChild(document.createElement("h3"));
+	monsterAlias.innerHTML = monster.alias;
+
+	// create list item containing color
+	const color = document.createElement("p");
+	color.className = "monster-color";
+	color.innerHTML = (`color: ${monster.color}`);
+	monsterProfile.appendChild(color);
+
+	// create ul containing monster stats
+	const stats = document.createElement("ul");
+	stats.className = "monster-stats";
+	monsterProfile.appendChild(stats);
+
+	// create list item containing eyes
+	const eyes = document.createElement("li");
+	eyes.className = "monster-eyes";
+	eyes.innerHTML = (`eyes: ${monster.stats.Eyes}`);
+	stats.appendChild(eyes);
+
+	// create list item containing horns
+	const horns = document.createElement("li");
+	horns.className = "monster-horns";
+	horns.innerHTML = (`horns: ${monster.stats.Horns}`);
+	stats.appendChild(horns);
+
+	// create list item containing tails
+	const tails = document.createElement("li");
+	tails.className = "monster-tails";
+	tails.innerHTML = (`tails: ${monster.stats.Tails}`);
+	stats.appendChild(tails);
+
+	// create list item containing tentacles
+	const tentacles = document.createElement("li");
+	tentacles.className = "monster-tentacles";
+	tentacles.innerHTML = (`tentacles: ${monster.stats.Tentacles}`);
+	stats.appendChild(tentacles);
+
+	// create list item containing legs
+	const legs = document.createElement("li");
+	legs.className = "monster-legs";
+	legs.innerHTML = (`legs: ${monster.stats.Legs}`);
+	stats.appendChild(legs);
+
+}
