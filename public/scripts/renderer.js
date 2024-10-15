7bf23f12ee931b4fb16057af47f90c365fd3d7e2
VonRiddarn <vonriddarn@gmail.com>
2024-10-15 18:46:28 +0200

    feat: moved edit logic away from renderer file

--- public/scripts/renderer.js
+++ public/scripts/renderer.js
@@ -115,38 +115,6 @@ function renderMonsterCards(arrayToRender)
   }
 };
 
-function toggleButtonSpans(card, isEditing)
-{
-	// Can be hard to understnad but is actually easy
-	// Create 2 variables and set them to the first 2 elements of the return of "array.from"
-	// We are deconstructing it instantly
-	const [deleteEditSpan, saveCancelSpan] = Array.from(card.querySelectorAll("span"));
-
-	card.className = isEditing ? "monster-card isediting" : "monster-card";
-	deleteEditSpan.style = isEditing ? "display: none;" : "";
-	saveCancelSpan.style = isEditing ? "" : "display: none;";
-}
-
-function enterEditMode(card)
-{
-	toggleButtonSpans(card, true);
-}
-
-function exitEditMode(card, saveChanges)
-{
-	toggleButtonSpans(card, false);
-}
-
-function deleteCard(card, object)
-{
-	card.remove();
-	monsters = monsters.filter((m) => m.uid !== object.uid);
-	console.log("--- MONSTER REMOVED ---");
-	console.log(object);
-	console.log(monsters);
-	console.log("--- --- ---");
-}
-
 renderMonsterCards(monsters);
 
 /*
